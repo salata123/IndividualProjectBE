403114ff62311d794e2bbadaf11f05360523fa40
Michal <salatman13@gmail.com>
2023-11-25 17:39:41 +0100

    first commit

---
+++ src/main/java/com/example/individualprojectbe/IndividualProjectBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.individualprojectbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualProjectBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndividualProjectBeApplication.class, args);
+    }
+
+}
