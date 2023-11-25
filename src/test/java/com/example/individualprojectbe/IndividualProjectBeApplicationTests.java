9541c377fa4b571cb420d7421c612c22d8206d02
Michal <salatman13@gmail.com>
2023-11-25 17:39:41 +0100

    first commit

---
+++ src/test/java/com/example/individualprojectbe/IndividualProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.individualprojectbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualProjectBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
